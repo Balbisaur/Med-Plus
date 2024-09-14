c4691e54e9625fd3b62690ba633cf24dcc8f4237
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-09-14 13:00:04 -0500

    Update reminder_model.py

--- models/reminder_model.py
+++ models/reminder_model.py
@@ -8,6 +8,9 @@ class Reminder(db.Model):
     medication_id = db.Column(db.Integer, db.ForeignKey('medications.id'), nullable=False)
     time_to_take = db.Column(db.DateTime, nullable=False)
     taken = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    dosage = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.string(50), nullable=False)
 
     def __init__(self, medication_id, time_to_take):
         self.medication_id = medication_id
