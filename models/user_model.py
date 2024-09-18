d85e285e77beccd15d9cd80a0705d643b9af8c26
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-09-18 02:58:14 -0500

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -1,16 +1,21 @@
 from database import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
+
 class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(20), nullable=False) # 'child', 'user', or 'caregiver'
-    email = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(20), nullable=False)# 'child', 'user', or 'caregiver'
+    email = db.Column(db.String(50), nullable=False)  
 
     medications = db.relationship('Medication', backref='owner', lazy=True)
+    prescriptions = db.relationship('Prescription', backref='user', lazy=True)
+    doctor = db.relationship('Doctor', backref='user', lazy=True)
+    reminders = db.relationship('Reminder', backref='user', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
