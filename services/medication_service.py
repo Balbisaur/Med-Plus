4aeccd5ccdbe0dc5825865d2cd9431bc69854d50
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-09-21 09:51:51 -0500

    Update medication_service.py

--- services/medication_service.py
+++ services/medication_service.py
@@ -1,19 +1,20 @@
 from models.medication_model import Medication
 from database import db
 
-def add_medication(data):
-    medication = Medication(
-        name=data['name'],
-        dosage=data['dosage'],
-        purpose=data['purpose'],
-        side_effects=data.get('side_effects'),
-        precautions=data.get('precautions'),
-        warnings=data.get('warnings'),
-        user_id=data['user_id']
-    )
-    db.session.add(medication)
-    db.session.commit()
-    return True
+class MedicationService:
+    def add_medication(self, user_id, data):
+        medication = Medication(
+            user_id=user_id,
+            medication_name=data.get('medication_name'),
+            dosage=data.get('dosage'),
+            purpose=data.get('purpose'),
+            side_effects=data.get('side_effects'),
+            precautions=data.get('precautions'),
+            warnings=data.get('warnings')
+        )
+        db.session.add(medication)
+        db.session.commit()
+        return {"id": medication.id, "medication_name": medication.medication_name, "dosage": medication.dosage}
 
-def get_medication_info(med_id):
-    return Medication.query.filter_by(id=med_id).first()
+    def get_medications(self, user_id):
+        return Medication.query.filter_by(user_id=user_id).all()
